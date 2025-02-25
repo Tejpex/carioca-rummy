d9263db62b70ed14a48ea48665a355ef71762e3f
Tejpex <equis_angel@hotmail.com>
2025-02-25 13:24:50 +0100

    Adjust header-layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,10 @@ export const Header = () => {
 
   return (
     <HeaderDiv>
-      <Title>Carioca Rummy</Title>
-      <Info>Mål: {contracts[contractNumber].name}</Info>
-      <Info>Att göra: {gameStages[gameStageIndex]}</Info>
+      
+        <Title>Carioca Rummy</Title>
+        <Info>Mål: {contracts[contractNumber].name} <br/>Att göra: {gameStages[gameStageIndex]}</Info>
+
       <ScoreDiv>
         <Score>Dina poäng: {player.score}</Score>
         <Score>Datorns poäng: {computer.score}</Score>
@@ -17,39 +18,46 @@ export const Header = () => {
   )
 }
 
-const Title = styled.h1`
-  margin: 0 10px;
-  font-size: 46px;
-  color: white;
-  font-family: "Quicksand", serif;
-  font-optical-sizing: auto;
-  font-weight: 700;
-  font-style: normal;
-`
-
 const Info = styled.h2`
   margin: 0 10px;
   font-size: 22px;
   color: white;
+  font-family: "Raleway", serif;
+  font-weight: 400;
+  font-style: normal;
+  text-align: left;
+  align-self: center;
 `
 
-const Score = styled.p`
-  font-size: 14px;
+
+const HeaderDiv = styled.div`
+  background-color: var(--secondary);
+  width: 100vw;
+  padding: 10px 30px;
+  display: grid;
+  grid-template-columns: 2fr 2fr 1fr;
+`
+
+const Title = styled.h1`
+  margin: 0 10px;
+  text-align: left;
+  font-size: 46px;
   color: white;
+  font-family: "Quicksand", serif;
+  font-weight: 700;
+  font-style: normal;
+  align-self: center;
 `
 
 const ScoreDiv = styled.div`
   display: flex;
   flex-direction: column;
-  margin-right: 80px;
+  align-items: start;
+  align-self: center;
 `
 
-const HeaderDiv = styled.div`
-  background-color: var(--secondary);
-  width: 100vw;
-  height: 80px;
-  padding: 10px 30px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+const Score = styled.p`
+  font-family: "Raleway", serif;
+  font-size: 14px;
+  color: white;
 `
