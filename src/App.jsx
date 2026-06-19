352851f5c3effeca3f7d9e90997bc6a550e14cab
Tejpex <equis_angel@hotmail.com>
2026-06-19 19:41:03 +0200

    Adjust outer margins

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import styled from "styled-components"
 import { CariocaProvider } from "./contexts/CariocaContext"
 import { Header } from "./components/Header"
 import { GameSite } from "./components/GameSite"
 
 function App() {
   return (
-    <div>
+    <AppContainer>
       <CariocaProvider>
         <Header />
         <GameSite />
       </CariocaProvider>
-    </div>
+    </AppContainer>
   )
 }
 
 export default App
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+`
