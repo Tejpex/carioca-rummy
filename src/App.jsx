a77c26c42da0f1cfedaea58bc2b7d07f99f68489
Tejpex <equis_angel@hotmail.com>
2026-06-19 08:47:17 +0200

    Fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { CariocaProvider } from "./contexts/CariocaContext"
 import { Header } from "./components/Header"
 import { GameSite } from "./components/GameSite"
-import "./App.css"
 
 function App() {
   return (
